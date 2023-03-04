a080cf25629c6c2ea8a7cae0ef0d1a56838f03c2
Rayhan Kabir <rayhankabir@iut-dhaka.edu>
2023-03-03 20:07:09 -0700

    upload

---
+++ cascade_forest.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import time
+from collections import defaultdict
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import accuracy_score
+import copy
+import sigdirect
+
+
+
+class Cascade:
+
+	def __init__(self):
+		pass
+
+	def build_cascade(self,X,y):
+		pass
